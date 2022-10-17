c188777a0fa0479152a801693d1eed94b1d1cdc9
Anna <114918694+asteponana@users.noreply.github.com>
2022-10-17 11:32:49 +0000

    1 uzd

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-men=int(input("Введи число, а я покажу, какой месяц ему соответствует: "))
-if men>12:
-  print("Такого месяца нет")
-if men<1:
-  print("Такого месяца нет")
-if men==1:print("январь")
-elif men==2:print("Февраль")
-elif men==3:print("Март")
-elif men==4:print("Апрель")
-elif men==5:print("Май")
-elif men==6:print("Июнь")
-elif men==7:print("Июль")
-elif men==8:print("Август")
-elif men==9:print("Сентябрь")
-elif men==10:print("Октябрь")
-elif men==11:print("Ноябрь")
-elif men==12:print("Декабрь")
+print("ievadi 3 leņķus")
+a=int(input("Ievadiet 1. leņķ: "))
+b=int(input("Ievadiet 2. leņķ: "))
+c=int(input("Ievadiet 3. leņķ: "))
+t=a+b+c
+if t>180:
+  print("tristuris neeksiste")
+if t<180:
+  print("tristuris neeksiste")
+elif a+b+c==180:
+  print("tristuris eksiste")
+if a==b==c: print("tas ir vienadmalu tristuris")
+elif a==90: print("tas ir taisnleņķa trijstūris")
+elif b==90: print("tas ir taisnleņķa trijstūris")
+elif c==90: print("tas ir taisnleņķa trijstūris")
+if a>90: print("tas ir platleņķa trijstūris")
+elif b>90: print("tas ir platleņķa trijstūris")
+elif c>90: print("tas ir platleņķa trijstūris")
+if a<90 and b<90 and c<90: print("tas ir šaurleņķa trijstūris")
