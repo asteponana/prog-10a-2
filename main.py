43916db83c8fb4811f8c376703016406249bf220
Anna <114918694+asteponana@users.noreply.github.com>
2022-12-12 13:02:43 +0000

    uzd 2

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-apples=int(input("Введи кол-во яблок (кг): "))
-колвокорицы=apples*20
-#на 1кг яблок нужно 20грамм корицы
-ценакорицы=колвокорицы/10*0.40
-#цена корицы 40 центов за 10 грамм
-колвосахар=apples*300
-#на 1кг яблок нужно 300 грамм сахара
-ценасахар=колвосахар/1000*0.99
-#цена сахара 0,99 центов за 1 кг
-колвовода=apples/10
-#на 1кг яблок нужно 100 миллилитров воды
-ценавода=колвовода*0.60
-#цена воды 60 центов за литр
-print("Кол-во корицы =", колвокорицы, "грамм")
-print("Цена корицы =", ценакорицы, "евро")
-print("Кол-во сахара =", колвосахар, "грамм")
-print("Цена сахара =", ценасахар, "евро")
-print("Кол-во воды =", колвовода, "литров")
-print("Цена воды =", ценавода, "евро")
-общаяцена=ценавода+ценасахар+ценакорицы
-print("Цена варенья =", общаяцена, "евро")
+kreklsk=int(input("Ievadiet kreklu daudzumu: "))
+kreklzim=int(input("Ievadiet printa tipu (teksts - 1, zime - 2, foto - 3): "))
+if kreklzim==1:
+  printcena = 5
+elif kreklzim==2:
+  printcena = 7
+elif kreklzim==3:
+  printcena = 20
+else:
+  printcena = 0
+  print("Nav tada printa, neiespējams veikt pasūtījumu ")
+pascena=kreklsk*printcena
+if pascena<50 and kreklzim>0 and kreklzim<4:
+  pascena=pascena+15
+  print("par piegādi papildus jāmaksā 15 EUR")
+print("Pasutijuma cena =", pascena, "euro") 
+if pascena>50 or pascena==50:
+  pascena=pascena
+  print("bezmaksas piegāde")
+if pascena>100:
+   print("Jums pienākas 5% atlaide! pasutijuma cena:", pascena/100*95,"euro")
