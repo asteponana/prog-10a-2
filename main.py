500404562c842ad2765befe7b1d1ea9deef91d5f
Anna <114918694+asteponana@users.noreply.github.com>
2022-12-19 12:04:11 +0000

    Uzd. 2.2

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-kreklsk=int(input("Ievadiet kreklu daudzumu: "))
-kreklzim=int(input("Ievadiet printa tipu (teksts - 1, zime - 2, foto - 3): "))
-if kreklzim==1:
-  printcena = 5
-elif kreklzim==2:
-  printcena = 7
-elif kreklzim==3:
-  printcena = 20
-else:
-  printcena = 0
-  print("Nav tada printa, neiespējams veikt pasūtījumu ")
-pascena=kreklsk*printcena
-if pascena<50 and kreklzim>0 and kreklzim<4:
-  pascena=pascena+15
-  print("par piegādi papildus jāmaksā 15 EUR")
-print("Pasutijuma cena =", pascena, "euro") 
-if pascena>50 or pascena==50:
-  pascena=pascena
-  print("bezmaksas piegāde")
-if pascena>100:
-   print("Jums pienākas 5% atlaide! pasutijuma cena:", pascena/100*95,"euro")
+import math
+RulPlat=int(input("Rulona platums: "))
+RulGar=int(input("Rulona garums: "))
+RulCena=int(input("Rulona Cenu: "))
+TelpasGarums=int(input("Telpa Garums: "))
+TelpasPlatums=int(input("Telpa platums: "))
+RulonaLaukums=RulGar*RulPlat
+telpasizmers = math.ceil(TelpasGarums) * math.ceil(TelpasPlatums)
+GridaCena=telpasizmers/RulonaLaukums*RulCena
+print("izklājot gridas cena:", GridaCena)
